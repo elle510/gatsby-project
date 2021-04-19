3215aa193f252aca8a2c25052774fc7704d2ac8d
elle510 <elle0510@gmail.com>
2021-04-19 22:13:16 +0900

    Generating Slugs for Posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,19 @@
  */
 
 // You can delete this file if you're not using it
+const path = require('path');
+
+module.exports.onCreateNode = ({ node, actions }) => {
+  const { createNode, createNodeField } = actions;
+
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = path.basename(node.fileAbsolutePath, '.md');
+    // console.log(JSON.stringify(node, undefined, 4));
+    // console.log('@@@@@@@@@@@@@@@@@', slug);
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug,
+    });
+  }
+};
